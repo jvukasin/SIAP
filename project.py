b67ff5475b240e59001623403395c37bd25f0f65
Vukasin <jovic.vukasin@gmail.com>
2020-03-18 20:32:38 +0100

    sve prebaceno u brojeve

--- project.py
+++ project.py
@@ -37,42 +37,47 @@ for i in countries:
 print(finalC)
 print(len(finalC))
 
+for index1, row1 in df.iterrows():
+    for contr in finalC:
+        if row1['country'] == contr:
+            df.at[index1, 'country'] = finalC.index(contr) + 1
+            break
 
-# for index1, row1 in df.iterrows():
-#     if row1['sex'] == 'male':
-#         df.at[index1, 'sex'] = 0
-#         row1['sex'] = 0
-#     elif row1['sex'] == 'female':
-#         df.at[index1, 'sex'] = 1
-#         row1['sex'] = 1
-#
-# for index1, row1 in df.iterrows():
-#     if row1['age'] == '5-14 years':
-#         df.at[index1, 'age'] = 1
-#     elif row1['age'] == '15-24 years':
-#         df.at[index1, 'age'] = 2
-#     elif row1['age'] == '25-34 years':
-#         df.at[index1, 'age'] = 3
-#     elif row1['age'] == '35-54 years':
-#         df.at[index1, 'age'] = 4
-#     elif row1['age'] == '55-74 years':
-#         df.at[index1, 'age'] = 5
-#     elif row1['age'] == '75+ years':
-#         df.at[index1, 'age'] = 6
-#
-# # PRAVLJENJE TRAIN I TEST SKUPA NA OSNOVU GODINA
-#
-# master_train = df[df['year'] >= 1990]
-# master_train = master_train[master_train['year'] <= 2008]
-#
-# master_test = df[df['year'] >= 2009]
-# master_test = master_test[master_test['year'] <= 2016]
-#
-#
-# """# Export podataka"""
-#
-# master_train.to_csv('master_train.csv')
-# master_test.to_csv('master_test.csv')
+for index1, row1 in df.iterrows():
+    if row1['sex'] == 'male':
+        df.at[index1, 'sex'] = 0
+        row1['sex'] = 0
+    elif row1['sex'] == 'female':
+        df.at[index1, 'sex'] = 1
+        row1['sex'] = 1
+
+for index1, row1 in df.iterrows():
+    if row1['age'] == '5-14 years':
+        df.at[index1, 'age'] = 1
+    elif row1['age'] == '15-24 years':
+        df.at[index1, 'age'] = 2
+    elif row1['age'] == '25-34 years':
+        df.at[index1, 'age'] = 3
+    elif row1['age'] == '35-54 years':
+        df.at[index1, 'age'] = 4
+    elif row1['age'] == '55-74 years':
+        df.at[index1, 'age'] = 5
+    elif row1['age'] == '75+ years':
+        df.at[index1, 'age'] = 6
+
+# PRAVLJENJE TRAIN I TEST SKUPA NA OSNOVU GODINA
+
+master_train = df[df['year'] >= 1990]
+master_train = master_train[master_train['year'] <= 2008]
+
+master_test = df[df['year'] >= 2009]
+master_test = master_test[master_test['year'] <= 2016]
+
+
+"""# Export podataka"""
+
+master_train.to_csv('master_train.csv')
+master_test.to_csv('master_test.csv')
 
 """# RF klasifikator"""
 
